6c33e5c7ec6d2eddee7ca56988f0a6424d20c9da
manlikexavy <chibokaxavier@gmail.com>
2024-10-10 19:27:37 +0100

    image preview fn working

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,20 +1,81 @@
 "use client";
+import uploadImageToCloudinary from "@/utils";
 import { InputText } from "primereact/inputtext";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaCloudUploadAlt } from "react-icons/fa";
+
+interface FormData {
+  name: string;
+  image: File | null | string | undefined;
+  description: string;
+  category: string;
+  price: number;
+}
 
 const page = () => {
   const [value, setValue] = useState("");
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    description: "",
+    category: "",
+    price: 0,
+    image: null,
+  });
+
+  const [photoPreview, setPhotoPreview] = useState<string | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    if (formData.image instanceof File) {
+      const objectUrl = URL.createObjectURL(formData.image);
+      setPhotoPreview(objectUrl);
+
+      // Clean up memory when the component unmounts or when the photo changes
+      return () => URL.revokeObjectURL(objectUrl);
+    } else if (typeof formData.image === "string") {
+      // If photo is a string (e.g., a URL from Cloudinary)
+      setPhotoPreview(formData.image);
+    } else {
+      setPhotoPreview(undefined);
+    }
+  }, [formData.image]);
+
+  const handleInputChange = (e: any) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleFileInputChange = async (e: any) => {
+    const file = e.target.files[0];
+    const data = await uploadImageToCloudinary(file);
+    setFormData({ ...formData, image: data?.url });
+  };
   return (
     <form className="mt-14 ml-10">
       <div className="flex flex-col">
         <div className="flex flex-col items-start mb-5">
-          {" "}
-          <p className="pb-3 font-semibold">Upload image</p>{" "}
-          <div className="h-[80px] w-[170px] bg-gray-100 flex flex-col justify-center items-center cursor-pointer">
-            <FaCloudUploadAlt className="text-5xl text-gray-400" />
-            <p className="text-xs">Upload</p>
-          </div>
+          {/* <p className="pb-3 font-semibold">Upload image</p>{" "} */}
+          <label htmlFor="customFile" className=" ">
+            Upload image
+          </label>
+          {formData.image ? (
+            <div className="h-[80px] w-[170px] bg-gray-100 flex flex-col justify-center items-center cursor-pointer">
+              <img src={photoPreview} alt="" />
+            </div>
+          ) : (
+            <div className="h-[80px] w-[170px] bg-gray-100 flex flex-col justify-center items-center cursor-pointer">
+              <FaCloudUploadAlt className="text-5xl text-gray-400" />
+              <p className="text-xs">Upload</p>
+            </div>
+          )}
+
+          <input
+            type="file"
+            name="photo"
+            id="customFile"
+            accept=".jpg,.png"
+             onChange={handleFileInputChange}
+            className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+          />
         </div>
         <div>
           <div className="mb-5">
@@ -73,7 +134,9 @@ const page = () => {
           </div>
         </div>
         <div className="mb-5">
-          <button className="flex justify-center p-4 bg-black text-white w-[200px] ">ADD</button>
+          <button className="flex justify-center p-4 bg-black text-white w-[200px] ">
+            ADD
+          </button>
         </div>
       </div>
     </form>
