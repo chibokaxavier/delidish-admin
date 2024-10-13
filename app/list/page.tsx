8d43675ce636c7d58db8ca18e4a5511a9aa5a849
manlikexavy <chibokaxavier@gmail.com>
2024-10-13 13:33:00 +0100

    fetching data (food) successful

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+"use client";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import { Toast } from "primereact/toast";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const toast = useRef<Toast>(null);
+  const showError = () => {
+    toast.current?.show({
+      severity: "error",
+      summary: "Error",
+      detail: "Error",
+      life: 3000,
+    });
+  };
+  const url = "http://localhost:4000";
 
-export default page
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const res = await axios.get(`${url}/api/food/list`);
+    console.log(res)
+    if (res.data.success) {
+      setList(res.data.data);
+    } else {
+      showError();
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+  return (
+    <div>
+      <Toast ref={toast} />
+    </div>
+  );
+};
+
+export default page;
